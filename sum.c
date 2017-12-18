09bebf6a0e0be1cb9c3d5515de97c17d52f26809
ArielMn22 <arielpaixao10@gmail.com>
2017-12-18 09:07:44 -0200

    sum

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int value1;
+	int value2;
+	int value3;
+
+	value1 = 50;
+	value2 = 50;
+	value3 = value1 + value2;
+
+	printf("A soma de %i e %i é igual a %i\n", value1, value2, value3);
+
+	return 0;
+
+}
