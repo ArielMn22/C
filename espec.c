d081031ef95f8fb3283a06685c5f57566958b9df
ArielMn22 <arielpaixao10@gmail.com>
2018-01-07 22:34:14 -0200

    ESPECIFICACOES EM C

---
+++ espec.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int help() {
+	printf("1 | Especificações da CPU\n");
+	printf("2 | Especificações da Memória\n");
+	printf("3 | Especificações do HD\n");
+	printf("4 | Especificações da Rede\n");
+	return 0;
+}
+
+int cpu() {
+  printf("__Especificações da CPU__\n\n");
+	system("lscpu");
+	return 0;
+}
+
+int memory() {
+  printf("__Especificações da Memória__\n\n");
+  system("cat /proc/meminfo");
+  return 0;
+}
+
+int hd() {
+  printf("__Particionamento do HD__\n\n");
+  system("lsblk");
+  return 0;
+}
+
+int net() {
+  printf("__ Teste de Conexão __\n\n");
+  system("ping -c 5 uol.com.br");
+  return 0;
+}
+
+int escolha(char* argv[]) {
+
+	int parametrum = 0;
+
+  parametrum = atoi( argv[1] );
+
+  system("cls || clear");
+
+	switch ( parametrum )
+	{
+		case 1:
+			cpu();
+			break;
+		case 2:
+			memory();
+			break;
+		case 3:
+			hd();
+			break;
+		case 4:
+			net();
+			break;
+		default :
+			help();
+	}
+
+  printf("\nAperte [QUALQUER TECLA] para sair\n");
+
+  system("read b");
+
+}
+
+int main(int argc, char* argv[]) {
+
+  if ( argc <= 1 )
+  {
+    printf("Adicione algum parâmetro:\n\n");
+    help();
+  } else {
+      escolha(argv);
+  }
+
+	return 0;
+}
