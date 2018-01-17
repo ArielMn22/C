dbcb41272a9c7ed6c8c27594551a5d428d39ee41
ArielMn22 <arielpaixao10@gmail.com>
2018-01-17 08:52:02 -0200

    Updating

--- calculadora.c
+++ calculadora.c
@@ -5,21 +5,49 @@
 
 float calculating() {	
 	
-		char batata[30];
+		char user_entry[30];
+		char op;
 		float num1 = 0;
 		float num2 = 0;
-		char op;
 		
+		char var1[30];
+    int number;
+
 		printf("> ");
-		scanf("%[^\n]s", batata);
+		scanf("%[^\n]s", user_entry);
 		setbuf(stdin, NULL);
-		//printf("Esse é o valor de 'batata': %s\n", batata);
+		
+		//printf("Esse é o valor de 'user_entry': %s\n", user_entry); //checando informações
 
-		sscanf(batata, "%f %c %f" , &num1, &op, &num2);
+		sscanf(user_entry, "%f %c %f" , &num1, &op, &num2) || sscanf(user_entry, "%s %c %d", var1, &op, &number) || sscanf(user_entry, "%s", var1);
+    /* A parte acima faz vários scans da váriavel "user_entry", desse modo o usuário pode colocar três tipos de entrada */
 
-		//printf("Esse é o valor de 'num1': %.2f\n", num1);
-		//printf("Esse é o valor de 'op': %c\n", op);
-		//printf("Esse é o valor de 'num2': %.2f\n", num2);
+    /*pointer*/
+    char *ponteirodevar;
+
+    ponteirodevar = var1;
+
+	//	printf("Esse é o valor de 'num1': %.2f\n", num1); //checando informações
+	//	printf("Esse é o valor de 'op': %c\n", op); //checando informações
+	//	printf("Esse é o valor de 'num2': %.2f\n", num2); //checando informações
+  //  printf("Esse é o valor de 'var1[5]': %s\n", var1); //checando informações
+  //  printf("Esse é o valor de '*ponteirodevar': %s\n", ponteirodevar); //checando informações
+
+		if ( op == '=' ) {
+
+			char user_var[30];
+			int user_equality;
+
+			sscanf(user_entry, "%s = %d", user_var, &user_equality);
+
+			//printf("valor de user_var: %s\n", user_var);
+			//printf("valor de user_equality: %d\n", user_equality);
+
+		}
+
+    if ( strcmp(ponteirodevar, "quit") == 0 ) {
+      exit(1);
+    }
 
 		switch( op )
 		{
@@ -41,17 +69,11 @@ float calculating() {
 
 int base() {
 
-	char batata[30];
-	
-	if (strcmp(batata, "quit") == 0)
-	{
-		exit(1);
-	} else {
-		calculating();
-		base();
+  while (1) {
+    calculating();  
 	}
 
-	}
+}
 
 int main(void) {
 	system("clear");
